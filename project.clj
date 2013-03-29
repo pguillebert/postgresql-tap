86d8fca4fa73191951a58b65bc2918019ab368bf
Philippe Guillebert <philippe@youmag.com>
2013-03-29 18:27:06 +0100

    Bump version of cascalog

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject postgresql-tap "1.0.0"
   :description "Postgresql Tap for cascading"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascalog "1.10.0"]
+                 [cascalog "1.10.1"]
                  [com.twitter/maple "0.2.4"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [org.apache.hadoop/hadoop-core "1.0.3"]]
