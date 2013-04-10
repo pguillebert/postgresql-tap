761d223c68bb8923806f704ffea2ac608c920dfa
Philippe Guillebert <philippe@youmag.com>
2013-04-10 11:42:04 +0200

    Allow pedantic

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject postgresql-tap "1.0.0"
   :description "Postgresql Tap for cascading"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascalog "1.10.1"]
+                 [cascalog "1.10.1" :exclusions [[cascalog/cascalog-elephantdb]
+                                                 [cascalog/midje-cascalog]
+                                                 [org.clojure/clojure]]]
                  [com.twitter/maple "0.2.4"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [org.apache.hadoop/hadoop-core "1.0.3"]]
