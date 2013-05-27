738b8d6512f1939d23d86d6f11485b511e4eff20
Philippe Guillebert <philippe@youmag.com>
2013-05-27 17:50:31 +0200

    Changed to be able to use as a sink tap

--- src/postgresql_tap/postgresql_tap.clj
+++ src/postgresql_tap/postgresql_tap.clj
@@ -1,43 +1,28 @@
 (ns postgresql-tap.postgresql-tap
   (:use [cascalog.api])
-  (:require [cascalog.ops :as c]
-            [clojure.string :as str])
-  (:import [com.twitter.maple.jdbc JDBCTap JDBCScheme TableDesc])
+  (:require [cascalog.ops :as c])
+  (:import [com.twitter.maple.jdbc JDBCTap JDBCScheme TableDesc]
+           [cascading.tuple Fields])
   (:gen-class))
 
 (defn postgresql-tap
-  [dbhost dbname dbusername dbpassword table primary-key columns limit concurrent-reads]
+  [dbhost dbname dbusername dbpassword table primary-key columns]
   (let [dburl (str "jdbc:postgresql://" dbhost "/" dbname)
-
-        tabledesc (com.twitter.maple.jdbc.TableDesc.
-                   table                         ;; @param tableName of type String
-                   (into-array columns)          ;; @param columnNames of type String[]
-                   (into-array columns)          ;; @param columnDefs of type String[]
-                   (into-array [ primary-key ])) ;; @param primaryKeys of type String
-
-        jdbcscheme (com.twitter.maple.jdbc.JDBCScheme.
-                    (into-array columns)                              ;; @param columns of type String[]
-                    (str "SELECT " (str/join ", " columns)            ;; \
-                         " FROM " table                               ;;  } @param selectQuery of type String
-                         " ORDER BY " primary-key " DESC")            ;; /
-                    (str "SELECT COUNT(" primary-key ") FROM " table) ;; @param countQuery of type String
-                    limit)                                            ;; @param limit of type long
-
-        tap (com.twitter.maple.jdbc.JDBCTap.
+        fields (into-array String columns)
+        primary (into-array String [primary-key])
+        qmark (fn [input]
+                (into-array String (map #(str "?" %) input)))
+        jdbcscheme (JDBCScheme. (Fields. (qmark fields)) fields
+                                primary
+                                (Fields. (qmark primary)) primary)
+        tabledesc (TableDesc. table (qmark fields) fields primary)
+        tap (JDBCTap.
              dburl
              dbusername
              dbpassword
              "org.postgresql.Driver"
              tabledesc
              jdbcscheme)]
-    (.setConcurrentReads tap concurrent-reads) ;; means 100 of limit 100 (limit / concurrent reads)
     tap))
 
-
-;; (def tap (postgresql-tap "lily" "youmag" "youmag_ro" "devvedro" "website" "id" ["id" "cache_images" "uri"] 100 2))
-;; (test-postgresql-tap tap (stdout))
-
-(defn- test-postgresql-tap
-  [input-tap output-tap]
-  (?<- output-tap [?id ?other ?uri]
-       (input-tap ?id ?other ?uri)))
+;; (def tap (postgresql-tap "lily" "youmag" "youmag_ro" "passw" "website" "id" ["id" "cache_images" "uri"]))
